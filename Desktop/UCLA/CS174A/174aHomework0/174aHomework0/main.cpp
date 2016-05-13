02cfb46a8d98b69dea6c744ff9083f6b0cd547cb
kimsvatos <kimsvatos@ucla.edu>
2016-05-12 21:31:37 -0700

    have a few scenes, birds not flying well scene to scene

---
+++ Desktop/UCLA/CS174A/174aHomework0/174aHomework0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  174aHomework0
+//
+//  Created by Kim Svatos on 4/7/16.
+//  Copyright (c) 2016 Kim Svatos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
